9e377e30b2ae2221498656a62e9f8553feea7faf
100PBaguuette <magnat.gregory@gmail.com>
2023-10-17 17:09:44 +0200

    heure 1709

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 Création d'un mini-jeu, premier projet de la semaine, le but est de mettre en pratique la POO vu lundi et mardi. 
 
 Projet de façon progressive : on va déjà produire une version 1.0, puis on passera à une version 2.0 un peu plus recherchée pour conclure sur une version 3.0 !
+
+Que faut-il pour faire un combat ? Moi j'ai en tête qu'il faut a minima :
+
+2 joueurs ;
+Que chaque joueur ait un niveau de vie donné ;
+Que ce niveau de vie baisse à chaque attaque subie ;
+Si la vie atteint zéro, le personnage est mort.
+
+attention reprendre RSPEC
---
+++ lib/player.rb
@@ -0,0 +1,32 @@
+class Player   #Création du joueur 1
+    attr_accessor :name, :life_points  #Avec nom et PV
+    
+    def initialize(name, life_points = 10)  #J'enregistre ça en instant grace à Init
+        @name = name
+        @life_points = life_points
+    end
+
+    def show_state  #J'affiche un petit message d'amour concernant mes players 
+        puts "La junkie / Le singe #{@name} possède #{@life_points} points de vie"
+    end
+
+    def gets_damage (take_dommage)  #Message en cas de dmg - mort héhé
+        @life_points -= take_dommage    #J'assigne une soustraction avec -=
+        @life_points <= 0 ? (puts "La junkie / Le singe #{@name} est MORT (Bolosse)!") : show_state #L'opérateur ternaire ? (...) : permet de controler grace au <=, et si ce n'est pas bon : renvoi ailleur 
+    end
+
+    def attacks
+        puts "Le noob #{self.name} attaque le joueur #{@name}"
+        damage = compute_damage   #création d'une variable de damage avec la méthode comput pour un damage aléatoire
+        @name.gets_damage(damage)  #mon joueur attaqué (@name.gets_damage) prend des damages grace (damage)
+        puts " Le/la bouuug inflige #{damage} points de dommages dans la tronche, iiiich"
+    end
+
+    def compute_damage
+        return rand(1..6)
+    end
+    
+
+end
+
+binding.pry
