15a2fc46e5a1232762587a0236d2ebbf1264585c
100PBaguuette <magnat.gregory@gmail.com>
2023-10-17 22:12:22 +0200

    grg

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-puts "Oyez ! Oyez ! Brave gens, nous voici tous réunis ici présent afin d'obserser avec nos yeux ébahit un combat !"
+puts " "
+puts "OYEZ ! OYEZ ! Brave gens, nous voici tous réunis ici présent afin d'obserser avec nos yeux ébahit un combat !"
+puts " " 
 
 puts "À ma droite 'Dorah'"
 player1 = Player.new("Dora")
@@ -12,17 +14,21 @@ player1 = Player.new("Dora")
 puts "À ma gauche 'Babouch'"
 player2 = Player.new("Babouch")
 
-puts "             \\ COMMENT SE PORTE NOS DEUX COMBATTANTS // "
+puts " "
+
+puts " \\ COMMENT SE PORTE NOS DEUX COMBATTANTS //"
 player1.show_state
 player2.show_state
 
 while player1.life_points > 0 && player2.life_points > 0 #boucle while qui part du player1 puis invoque les life poits, si > à 0 et pareil pour le deux 
-    
-    puts "FIGHT"
 
+    puts " "
+    puts "FIGHT FIGHT FIGHT FIGHT FIGHT FIGHT FIGHT FIGHT"
     player1.attacks(player2)
     break if player2.life_points <= 0
     player2.attacks(player1)
+
+    puts " "
 
     player1.show_state
     player2.show_state
